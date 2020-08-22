5fd1e2a9cd96dd747427313194f8b6162c8d1f55
Joel Dsouza <64890495+joel06-dsouza@users.noreply.github.com>
2020-08-22 13:34:17 +0530

    Add files via upload

---
+++ Year.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class Year{
+ public static void main(String args[]){
+  int year;
+  Scanner S = new Scanner(System.in);
+  System.out.print("Enter the year to check weather it is a leap or not:....");
+  year = S.nextInt();
+
+  if(year%4 == 0)
+  {
+   if(year%100 == 0)
+    {
+      if(year%400 == 0)
+      { 
+        System.out.println("It is a leap year.");  
+      }
+      else
+      {
+        System.out.print("It is not a leap year.");
+      }
+    }
+   else
+    { 
+        System.out.println("It is a leap year.");  
+    }
+  }
+ else
+  {
+     System.out.print("It is not a leap year.");
+  }s
+ }
+}
